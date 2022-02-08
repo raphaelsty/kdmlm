26f98284de685fec67b9bfcb7577949269da1a31
Raphael Sourty <raphael.sourty@gmail.com>
2022-02-08 18:40:33 +0100

    Upvate expand vocabulary function

--- kdmlm/utils/vocabulary.py
+++ kdmlm/utils/vocabulary.py
@@ -1,3 +1,5 @@
+import json
+
 import torch
 import tqdm
 
@@ -5,9 +7,10 @@ __all__ = ["expand_bert_vocabulary"]
 
 
 def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
-    """Expand bert vocabulary by adding entities to bert embeddings. Entities weights are
+    """Expand Transformers vocabulary by adding entities to bert embeddings. Entities weights are
     initialized by computing the mean of the embeddings of the tokens of the entities. This
-    function is made for models which use word piece tokenizer.
+    function is made for models which use word piece tokenizer. Also compatible with Roberta
+    which has a different behaviour.
 
     Parameters
     ----------
@@ -44,16 +47,16 @@ def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
     ...         break
     ...     else:
     ...         print(top["sequence"])
-    i have studied at harvard.
-    i have studied at yale.
     i have studied at oxford.
     i have studied at ucla.
-    i have studied at mit.
-    i have studied at nyu.
     i have studied at princeton.
     i have studied at university.
     i have studied at stanford.
     i have studied at cambridge.
+    i have studied at byu.
+    i have studied at berkeley.
+    i have studied at columbia.
+    i have studied at harrow.
 
     >>> len(tokenizer)
     30522
@@ -64,7 +67,7 @@ def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
     ...         n += 1
 
     >>> n
-    6342
+    2208
 
     >>> len(kb.mentions.values())
     14541
@@ -82,7 +85,7 @@ def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
     ...         n += 1
 
     >>> n
-    14535
+    14531
 
     >>> pipeline = FillMaskPipeline(model = model, tokenizer = tokenizer, framework = "pt")
     >>> y_pred = pipeline("I have studied at [MASK].", targets=list(kb.mentions.values()), top_k=10)
@@ -92,40 +95,62 @@ def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
     ...         break
     ...     else:
     ...         print(top["sequence"])
-    i have studied at harvard.
-    i have studied at yale.
     i have studied at oxford.
+    i have studied at harvarduniversity.
     i have studied at ucla.
-    i have studied at mit.
-    i have studied at nyu.
     i have studied at princeton.
+    i have studied at yaleuniversity.
+    i have studied at princetonuniversity.
     i have studied at university.
+    i have studied at stanforduniversity.
+    i have studied at harvardcollege.
     i have studied at stanford.
-    i have studied at cambridge.
 
     References
     ----------
     1. (tokenizer is slow after adding new tokens #615)[https://github.com/huggingface/tokenizers/issues/615]
 
     """
+    from transformers import RobertaForMaskedLM
+
     entities_to_add = {}
     vocabulary = tokenizer.get_vocab()
 
     # Expand bert vocabulary for entities that are not already part of it's vocabulary:
     for e in tqdm.tqdm(entities, position=0, desc="Adding entities to Bert vocabulary."):
         if len(tokenizer.tokenize(e)) > 1:
-            entities_to_add[e] = tokenizer.tokenize(original_entities[e])
+            e_tkzn = tokenizer.tokenize(original_entities[e])
+            if isinstance(model, RobertaForMaskedLM):
+                e = f"Ġ{e}"
+            entities_to_add[e] = e_tkzn
 
     tokenizer.save_pretrained(".")
 
-    with open("vocab.txt", "a", encoding="utf-8") as tokenizer_vocab:
+    with open("vocab.txt", "a") as tokenizer_vocab:
         for token in entities_to_add:
             tokenizer_vocab.write(f"{token}\n")
+
+    if isinstance(model, RobertaForMaskedLM):
+        with open("vocab.json", "r") as input_vocab:
+            vocabulary = json.load(input_vocab)
+
+        for e in tqdm.tqdm(
+            entities_to_add, position=0, desc="Roberta vocab.json", total=len(entities_to_add)
+        ):
+            size_vocabulary = max(list(vocabulary.values()))
+            if e not in vocabulary:
+                vocabulary[e] = size_vocabulary + 1
+
+        with open("vocab.json", "w") as output_vocab:
+            json.dump(vocabulary, output_vocab, indent=4, ensure_ascii=False)
+
+        with open("merges.txt", "a", encoding="utf-8") as tokenizer_vocab:
+            for token in entities_to_add:
+                tokenizer_vocab.write(f"Ġ{token}\n")
 
     tokenizer = tokenizer.from_pretrained(".")
     vocabulary = tokenizer.get_vocab()
     model.resize_token_embeddings(len(tokenizer))
-
     # Initialize smart weights for entities thar are now part of the vocabulary of Bert.
     # The embeddings of Toulouse University will be equal to the mean of the embeddings of
     # Toulouse and University.
@@ -142,7 +167,6 @@ def expand_bert_vocabulary(model, tokenizer, entities, original_entities):
                     )
 
                     for token in tokens:
-                        # Part of bert vocabulary
                         mean_vector += layer[vocabulary[token]].detach().clone()
 
                     layer[vocabulary[e]] = mean_vector / len(tokens)
